dd3b31b5896db89d0e537865a83653e5cab04794
Tzu-Chao Wang <john29917958@gmail.com>
2022-09-20 23:25:09 +0800

    Implement the +, -, *, / and "to float" features
    Bind the callback function to click event of each operator button and implement the addition, subtraction, multiplication, and division functions. Bind a callback function to the . button and implement the function of converting the integer operand value to a float number.

--- calculator.js
+++ calculator.js
@@ -5,11 +5,34 @@ window.onload = function () {
         elem: document.getElementById('operand-1'),
         value: null,
         get isSet() { return this.value != null; },
+        set: function (value) {
+            value = value.toString();
+            this.value = value;
+            this.elem.textContent = value;
+        },
+        toFloat: function () {
+            if (this.isSet && this.value.includes('.')) {
+                return;
+            }
+
+            if (this.isSet) {
+                this.value += '.';
+            } else {
+                this.value = '0.';
+            }
+
+            this.elem.textContent = this.value;
+        },
+        unset: function () {
+            this.value = null;
+            this.elem.textContent = '';
+        },
         append: function (value) {
-            if (!this.value) {
+            value = value.toString();
+            if (!this.isSet || this.value === '0') {
                 this.value = value;
             } else {
-                this.value = this.value * 10 + value;
+                this.value += value;
             }
             this.elem.textContent = this.value;
         }
@@ -19,23 +42,65 @@ window.onload = function () {
         get isSet() { return this.value != null; },
         set: function (value) {
             this.value = value;
-            this.elem.textContent = value;
+            switch (value) {
+                case '+':
+                    this.elem.className = 'fa-solid fa-plus fa-2xs';
+                    break;
+                case '-':
+                    this.elem.className = 'fa-solid fa-minus fa-2xs';
+                    break;
+                case '*':
+                    this.elem.className = 'fa-solid fa-xmark fa-2xs';
+                    break;
+                case '/':
+                    this.elem.className = 'fa-solid fa-divide fa-2xs';
+                    break;
+                default:
+                    throw `Unsupported operator ${value}.`
+            }
+        },
+        unset: function () {
+            this.value = null;
+            this.elem.className = '';
         }
     }, operand2 = {
         elem: document.getElementById('operand-2'),
-        get isSet() { return this.value != null; },
         value: null,
+        get isSet() { return this.value != null; },
+        set: function (value) {
+            value = value.toString();
+            this.value = value;
+            this.elem.textContent = value;
+        },
+        toFloat: function () {
+            if (this.isSet && this.value.includes('.')) {
+                return;
+            }
+
+            if (this.isSet) {
+                this.value += '.';
+            } else {
+                this.value = '0.';
+            }
+
+            this.elem.textContent = this.value;
+        },
+        unset: function () {
+            this.value = null;
+            this.elem.textContent = '';
+        },
         append: function (value) {
-            if (!this.value) {
+            value = value.toString();
+            if (!this.isSet || this.value === '0') {
                 this.value = value;
             } else {
-                this.value = this.value * 10 + value;
+                this.value = this.value += value;
             }
             this.elem.textContent = this.value;
         }
     };
 
-    operand1.append(0);
+    operand1.set(0);
 
     let numBtns = document.querySelectorAll('.num-btn');
     for (const numBtn of numBtns) {
@@ -55,8 +120,41 @@ window.onload = function () {
         operatorBtn.addEventListener('click', function (e) {
             let op = this.getAttribute('data-op');
             if (operand1.isSet) {
-                operator.set(op)
+                operator.set(op);
             }
         });
     }
+
+    document.getElementById('equal-btn').addEventListener('click', function (e) {
+        if (operand1.isSet && operator.isSet && operand2.isSet) {
+            let result = 0;
+            switch (operator.value) {
+                case '+':
+                    result = Number(operand1.value) + Number(operand2.value);
+                    break;
+                case '-':
+                    result = Number(operand1.value) - Number(operand2.value);
+                    break;
+                case '*':
+                    result = Number(operand1.value) * Number(operand2.value);
+                    break;
+                case '/':
+                    result = Number(operand1.value) / Number(operand2.value);
+                    break;
+                default:
+                    throw `Unsupported operator ${operator.value}.`;
+            }
+            operand1.set(result);
+            operator.unset();
+            operand2.unset();
+        }
+    });
+
+    document.getElementById('dot-btn').addEventListener('click', function (e) {
+        if (operator.isSet) {
+            operand2.toFloat();
+        } else {
+            operand1.toFloat();
+        }
+    });
 }
