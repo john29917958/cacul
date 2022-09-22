d3cd4643d03272daa5771617641375b04eefe4d8
Tzu-Chao Wang <john29917958@gmail.com>
2022-09-22 08:00:22 +0800

    Implement the functionality of representing the floating point value by the scientific notation

--- calculator.js
+++ calculator.js
@@ -243,11 +243,15 @@ window.onload = function () {
         }
 
         let number = Math.pow(Number(operand.value), 2);
-        if (operand.value.includes('.')) {
+        if (number.toString() === '0') {
+            operand.set(0);            
+        } else if (!number.toString().includes('e') && operand.value.includes('.')) {            
             let fractionalPartDigits = operand.value.split('.')[1].length * 2;
             number = number.toFixed(fractionalPartDigits);
+            operand.set(number);
+        } else {
+            operand.set(number);
         }
-        operand.set(number);
     });
 
     document.getElementById('reciprocal-btn').addEventListener('click', function () {
